d151ca0d17a2e853ebd2bd76b19127ea1bdd1f4e
tonijigan <100856397+tonijigan@users.noreply.github.com>
2024-10-25 15:47:28 +0300

    Добавил рекламму

---
+++ Assets/Scripts/SDK/SDKPromotionalVideo.cs
@@ -0,0 +1,34 @@
+using System;
+using Agava.YandexGames;
+using UnityEngine;
+
+namespace SDK
+{
+    public class SDKPromotionalVideo : MonoBehaviour
+    {
+        private const int MinValue = 0;
+        private const int MaxValue = 1;
+
+        [SerializeField] private FocusTracking _focusTracking;
+
+        public event Action ClosedCallBack;
+
+        public void ShowInterstitialAd() =>
+            InterstitialAd.Show(OnOpenCallBack, OnCloseCallBack);
+
+        private void OnOpenCallBack()
+        {
+            Time.timeScale = MinValue;
+            _focusTracking.enabled = false;
+            AudioListener.pause = true;
+        }
+
+        private void OnCloseCallBack(bool wasShown)
+        {
+            Time.timeScale = MaxValue;
+            _focusTracking.enabled = true;
+            AudioListener.pause = false;
+            ClosedCallBack();
+        }
+    }
+}
--- Assets/Scripts/TestScripts/TestButtonLoss.cs
+++ Assets/Scripts/TestScripts/TestButtonLoss.cs
@@ -1,8 +1,34 @@
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using SDK;
+using UnityEngine.UI;
 
-public class TestButtonLoss : AbstractButton
+[RequireComponent(typeof(Button))]
+public class TestButtonLoss : MonoBehaviour
 {
-    protected override void OnClick()
+    [SerializeField] private SDKPromotionalVideo _promotionalVideo;
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(PlayPromo);
+        _promotionalVideo.ClosedCallBack += OnClick;
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(PlayPromo);
+        _promotionalVideo.ClosedCallBack -= OnClick;
+    }
+
+    private void PlayPromo() => _promotionalVideo.ShowInterstitialAd();
+
+    public void OnClick()
     {
         SceneManager.LoadScene(Enums.ScenesName.ChooseLevel.ToString());
     }
