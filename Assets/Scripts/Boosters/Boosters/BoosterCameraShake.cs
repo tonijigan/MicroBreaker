47673b9cf6f7078e9dbab8ccba3d24fa83c8efd5
tonijigan <100856397+tonijigan@users.noreply.github.com>
2024-11-06 18:55:12 +0300

    Сделал дестабилизацию камеры и добавил камуре тряску при ударе о кубы

--- Assets/Scripts/Boosters/Boosters/BoosterCameraShake.cs
+++ Assets/Scripts/Boosters/Boosters/BoosterCameraShake.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class BoosterCameraShake : AbstractBooster
 {
+    [SerializeField] private CameraMoveShake _cameraMoveShake;
+
     public override void OnStartAction(BoosterEffect boosterEffect)
     {
-
+        _cameraMoveShake.Destabilization();
+        boosterEffect.SetActionActive();
     }
 
     public override void StopAction(BoosterEffect boosterEffect)
     {
-
+        boosterEffect.SetActionActive();
     }
 }
---
+++ Assets/Scripts/CameraMoveShake.cs
@@ -0,0 +1,111 @@
+using BoxObject;
+using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+public class CameraMoveShake : MonoBehaviour
+{
+    private const float MaxDelta = 25f;
+    private const float NewPositionX = 20;
+    private const float MinValue = 0;
+    private const float ShakeTimer = 0.3f;
+
+    [SerializeField] private CinemachineVirtualCamera _camera;
+    [SerializeField] private LocationCreate _locationCreate;
+    [SerializeField] private BoxesFalling _boxesFalling;
+
+    private Coroutine _coroutineMove;
+    private Coroutine _coroutineShake;
+    private CinemachineBasicMultiChannelPerlin _multiChannelPerlin;
+    private CinemachineTransposer _transposer;
+
+    private void Awake()
+    {
+        _multiChannelPerlin = _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _transposer = _camera.GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+    private void OnEnable()
+    {
+        _locationCreate.Inited += onInit =>
+        {
+            foreach (Box box in _locationCreate.CurrentLocation.BoxContainer.Boxes)
+                box.Died += Shake;
+
+            foreach (Box box in _boxesFalling.Boxes)
+                box.Died += Shake;
+        };
+    }
+
+    private void OnDisable()
+    {
+        _locationCreate.Inited += onInit =>
+        {
+            foreach (Box box in _locationCreate.CurrentLocation.BoxContainer.Boxes)
+                box.Died -= Shake;
+
+            foreach (Box box in _boxesFalling.Boxes)
+                box.Died -= Shake;
+        };
+    }
+
+    public void Shake()
+    {
+        if (_coroutineShake != null)
+            StopCoroutine(_coroutineShake);
+
+        _coroutineShake = StartCoroutine(PlayShake());
+    }
+
+    public void Destabilization()
+    {
+        if (_coroutineMove != null)
+            StopCoroutine(_coroutineMove);
+
+        _coroutineMove = StartCoroutine(Move());
+    }
+
+    public void Stabilization() => _transposer.m_FollowOffset.x = 0f;
+
+    private IEnumerator PlayShake()
+    {
+        float intensety = 1;
+        _multiChannelPerlin.m_AmplitudeGain = intensety;
+        yield return new WaitForSeconds(ShakeTimer);
+        _multiChannelPerlin.m_AmplitudeGain = MinValue;
+    }
+
+    private IEnumerator Move()
+    {
+        while (_transposer.m_FollowOffset.x != NewPositionX)
+        {
+            _transposer.m_FollowOffset.x = GetPositionX(NewPositionX);
+            yield return null;
+        }
+
+        while (_transposer.m_FollowOffset.x != -NewPositionX)
+        {
+            _transposer.m_FollowOffset.x = GetPositionX(-NewPositionX);
+            yield return null;
+        }
+
+        while (_transposer.m_FollowOffset.x != NewPositionX)
+        {
+            _transposer.m_FollowOffset.x = GetPositionX(NewPositionX);
+            yield return null;
+        }
+
+        while (_transposer.m_FollowOffset.x != MinValue)
+        {
+            _transposer.m_FollowOffset.x = GetPositionX(MinValue);
+            yield return null;
+        }
+
+        StopCoroutine(_coroutineMove);
+    }
+
+    private float GetPositionX(float newPositionX)
+    {
+        return Mathf.MoveTowards(_transposer.m_FollowOffset.x, newPositionX, MaxDelta * Time.deltaTime);
+    }
+}
