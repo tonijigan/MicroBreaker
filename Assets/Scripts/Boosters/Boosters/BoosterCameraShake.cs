8a40cdf231055160feff8c09d5c059a619e7013d
tonijigan <100856397+tonijigan@users.noreply.github.com>
2024-11-06 16:04:51 +0300

    Делаю буст безконтрольная камера

---
+++ Assets/Scripts/Boosters/Boosters/BoosterCameraShake.cs
@@ -0,0 +1,15 @@
+using Boosters;
+using UnityEngine;
+
+public class BoosterCameraShake : AbstractBooster
+{
+    public override void OnStartAction(BoosterEffect boosterEffect)
+    {
+
+    }
+
+    public override void StopAction(BoosterEffect boosterEffect)
+    {
+
+    }
+}
