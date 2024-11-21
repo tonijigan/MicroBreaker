5e51108e9d513dcb669b3b877d182356c49700f8
tonijigan <100856397+tonijigan@users.noreply.github.com>
2024-11-22 00:23:46 +0300

    Поправил движение шарика и его взаимодействие с платформой

--- Assets/Scripts/BallObject/BallMovement.cs
+++ Assets/Scripts/BallObject/BallMovement.cs
@@ -10,29 +10,30 @@ namespace BallObject
     [RequireComponent(typeof(Rigidbody), typeof(Ball), typeof(BallEffect))]
     public class BallMovement : MonoBehaviour
     {
-        private const int FirstCount = 0;
+        private const int Divider = 2;
+        private const int MinValue = 0;
         private const int MaxSpeed = 4000;
-        private const int StandartSpeed = 1000;
         private const int Damage = 1;
         private const int CountDistance = 15;
         private const float GravityValue = 0.1f;
         private const float PositionZero = 0f;
-        private const float GravityPositionZ = 0.02f;
+        private const float GravityPositionZ = 0.002f;
 
         [SerializeField] private Platform _platformTargetGravity;
         [SerializeField] private Transform _ballPoint;
         [SerializeField] private BallSound _ballSound;
-        [SerializeField] private float _speedForce;
+        [SerializeField] private float _speed;
 
         public event Action BoxTriggered;
 
         private BallEffect _ballEffect;
         private Rigidbody _rigidbody;
-        private Vector3 _lastVelosity;
+        private Vector3 _currentDirection = Vector3.forward;
         private Vector3 _lastPlatformPosition;
+        private Vector3 _lastVelosity;
         private Ball _ball;
         private bool _isGravityPlatform = false;
-        private float _currentSpeed = StandartSpeed;
+        private float _startSpeed;
 
         public Transform Transform { get; private set; }
 
@@ -42,13 +43,10 @@ namespace BallObject
             _rigidbody = GetComponent<Rigidbody>();
             _ball = GetComponent<Ball>();
             Transform = transform;
+            _startSpeed = _speed;
         }
 
-        private void OnEnable() => _ball.Actived += OnMove;
-
-        private void OnDisable() => _ball.Actived -= OnMove;
-
-        private void Update()
+        private void FixedUpdate()
         {
             if (_ball.IsActive == false)
             {
@@ -60,14 +58,16 @@ namespace BallObject
             _lastVelosity = _rigidbody.velocity;
             _lastPlatformPosition = _platformTargetGravity.transform.position;
 
-            _rigidbody.velocity += new Vector3(PositionZero, PositionZero, -GravityPositionZ);
+            _currentDirection += new Vector3(PositionZero, PositionZero, -GravityPositionZ);
 
             if (_isGravityPlatform == true)
             {
                 if (Vector3.Distance(_platformTargetGravity.transform.position, Transform.position) > CountDistance) return;
                 Vector3 direction = (_platformTargetGravity.transform.position - Transform.position).normalized;
-                _rigidbody.velocity += new Vector3(direction.x * GravityValue, PositionZero, PositionZero);
+                _currentDirection += new Vector3(direction.x * GravityValue, PositionZero, PositionZero);
             }
+
+            Move(_currentDirection, _speed);
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -75,8 +75,9 @@ namespace BallObject
             if (_ball.IsActive == false)
                 return;
 
+            SetSpeed();
             Collision currentCollision = collision;
-            Vector3 currentPoint = currentCollision.contacts[FirstCount].point;
+            Vector3 currentPoint = currentCollision.contacts[MinValue].point;
 
             if (currentCollision.gameObject.TryGetComponent(out ITrigger trigger))
             {
@@ -84,12 +85,12 @@ namespace BallObject
 
                 if (currentCollision.gameObject.TryGetComponent(out Fence fence) && fence.IsOpenPortal == true)
                 {
-                    fence.Relocate(this, currentPoint, GetCurrentDirection(currentCollision));
+                    fence.Relocate(this, currentPoint, _currentDirection);
                     _ballSound.Play(fence.GetClip());
                     return;
                 }
 
-                Move(GetCurrentDirection(currentCollision), GetCurrentSpeed(trigger.GetSpeed()));
+                _currentDirection = Vector3.Reflect(_lastVelosity, collision.contacts[MinValue].normal).normalized;
             }
 
             if (currentCollision.gameObject.TryGetComponent(out ISound sound))
@@ -102,38 +103,35 @@ namespace BallObject
             }
         }
 
-        public void SetGravity() => _isGravityPlatform = !_isGravityPlatform;
-
-        public void SetMaxSpeed() => _currentSpeed = MaxSpeed;
-
-        public void SetStandartSpeed() => _currentSpeed = StandartSpeed;
-
-        public void Move(Vector3 direction) => Move(direction, _currentSpeed);
-
-        private void FollowToPointPosition() => Transform.position = _ballPoint.position;
-
-        private void Move(Vector3 direction, float speed) => _rigidbody.velocity = speed * Time.deltaTime * direction;
-
-        private void OnMove() => _rigidbody.AddForce(_speedForce * Vector3.forward, ForceMode.Impulse);
-
-        private float GetCurrentSpeed(float value)
+        private void OnCollisionStay(Collision collision)
         {
-            if (value < _currentSpeed) return _currentSpeed;
-            else return value;
-        }
-
-        private Vector3 GetCurrentDirection(Collision collision)
-        {
-            Vector3 direction = Vector3.zero;
-            Vector3 reflect = Vector3.Reflect(_lastVelosity.normalized, collision.contacts[FirstCount].normal);
+            float degreeSpeed = 12;
 
             if (collision.gameObject.TryGetComponent(out Platform platform))
             {
-                direction = (_platformTargetGravity.transform.position - _lastPlatformPosition).normalized;
-                _lastPlatformPosition = Vector3.zero;
+                _speed += degreeSpeed *= degreeSpeed;
+                Vector3 newDirection = _lastPlatformPosition - _platformTargetGravity.transform.position;
+                _currentDirection = (_currentDirection - newDirection).normalized;
             }
+        }
 
-            return new Vector3(direction.x + reflect.x, reflect.y, reflect.z);
+        public void SetGravity() => _isGravityPlatform = !_isGravityPlatform;
+
+        public void SetMaxSpeed() => _speed = MaxSpeed;
+
+        public void SetStandartSpeed() => _speed = _startSpeed;
+
+        public void Move(Vector3 direction) => Move(direction, _speed);
+
+        private void FollowToPointPosition() => Transform.position = _ballPoint.position;
+
+        private void Move(Vector3 direction, float speed) => _rigidbody.velocity = speed * Time.fixedDeltaTime * direction;
+
+        private void SetSpeed()
+        {
+            if (_speed < MinValue) _speed = _startSpeed;
+
+            if (_speed > _startSpeed) _speed -= (_speed - _startSpeed) / Divider;
         }
     }
 }
